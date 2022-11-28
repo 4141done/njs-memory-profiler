0f714f62bd2d221b6f124fda2af5d4601987cf37
Javier Evans <j.evans@f5.com>
2022-11-28 09:27:44 -0800

    chore: fix install script for test

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,18 +1,27 @@
 const fs = require("fs/promises");
-console.log(`install script excuting from ${__dirname}`);
+console.log(`njs-memory-profiler install script excuting from ${__dirname}`);
 const path = require("path");
 const package = require("../package.json");
 
 // This is the user's local directory
-const projectRoot = process.env.INIT_CWD;
+const projectRoot = process.env.INIT_CWD || "./";
 
-const dest =
-  process.env["NJS_MODULES_DIR"] || path.resolve(projectRoot, "/njs_modules");
+// We'll assume that `njs_modules` is where all modules will go but let the user
+// override if desired
+const moduleBase =
+  process.env["NJS_MODULES_DIR"] || path.resolve(projectRoot, "./njs_modules");
 
-try {
-  await fs.access(dest);
-} catch {
-  await mkdir(dest, { recursive: true });
+// Each module should be in its own folder in case it has multiple files
+const dest = path.join(moduleBase, "njs-memory-profiler");
+
+async function doCopy() {
+  try {
+    await fs.access(dest);
+  } catch {
+    await fs.mkdir(dest, { recursive: true });
+  }
+
+  await fs.copyFile(package.main, `${dest}/njs-memory-profiler.mjs`);
 }
 
-await fs.copyFile(path.resolve("..", package.main), dest);
+doCopy();
